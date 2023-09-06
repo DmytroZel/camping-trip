b38a7530cb954fd80c2692e17bbaf9be7f0aebde
Dmytro <d.zel@lampalampa.net>
2023-09-06 15:04:19 +0300

    add new login page, add trip view

---
+++ lib/gen/colors.gen.dart
@@ -0,0 +1,96 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
+
+import 'package:flutter/painting.dart';
+import 'package:flutter/material.dart';
+
+class ColorName {
+  ColorName._();
+
+  /// Color: #000000
+  static const Color black = Color(0xFF000000);
+
+  /// Color: #464444
+  static const Color blackBottomSheet = Color(0xFF464444);
+
+  /// Color: #171515
+  static const Color blackInput = Color(0xFF171515);
+
+  /// Color: #0500FF
+  static const Color blue = Color(0xFF0500FF);
+
+  /// Color: #667080
+  static const Color darkGrey = Color(0xFF667080);
+
+  /// Color: #D1D0D0
+  static const Color disabledBackground = Color(0xFFD1D0D0);
+
+  /// Color: #747373
+  static const Color disabledText = Color(0xFF747373);
+
+  /// Color: #FF3B30
+  static const Color errorColor = Color(0xFFFF3B30);
+
+  /// Color: #55FF7A
+  static const Color green = Color(0xFF55FF7A);
+
+  /// Color: #A2A2A2
+  static const Color grey = Color(0xFFA2A2A2);
+
+  /// Color: #F7F7F7
+  static const Color greyInput = Color(0xFFF7F7F7);
+
+  /// Color: #EEF1F4
+  static const Color lightGrey = Color(0xFFEEF1F4);
+
+  /// Color: #FFD9F1
+  static const Color lightPink = Color(0xFFFFD9F1);
+
+  /// Color: #E8E1FF
+  static const Color lightPurple = Color(0xFFE8E1FF);
+
+  /// Color: #d6e2ea
+  static const Color loginBG = Color(0xFFD6E2EA);
+
+  /// Color: #FF7BF2
+  static const Color pink = Color(0xFFFF7BF2);
+
+  /// Color: #FFECF8
+  static const Color pinkButton = Color(0xFFFFECF8);
+
+  /// Color: #CC7FAF
+  static const Color pinkButtonTitle = Color(0xFFCC7FAF);
+
+  /// Color: #FF98DE
+  static const Color pinkRegistration = Color(0xFFFF98DE);
+
+  /// Color: #8D67FD
+  static const Color purple = Color(0xFF8D67FD);
+
+  /// Color: #E8E1FF
+  static const Color purpleConnectBG = Color(0xFFE8E1FF);
+
+  /// Color: #D1C2FE
+  static const Color purpleConnectBGLine = Color(0xFFD1C2FE);
+
+  /// Color: #A486FD
+  static const Color purpleSuccessSub = Color(0xFFA486FD);
+
+  /// Color: #FF9574
+  static const Color red = Color(0xFFFF9574);
+
+  /// Color: #FFF2C7
+  static const Color tokenBG = Color(0xFFFFF2C7);
+
+  /// Color: #8DF8FF
+  static const Color turquoise = Color(0xFF8DF8FF);
+
+  /// Color: #FFFFFF
+  static const Color white = Color(0xFFFFFFFF);
+}
